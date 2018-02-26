9f75c21c189f77dbdbf92f6e174b1c3f64de3ba5
Theron Halsey <theron.halsey@gmail.com>
2018-02-25 23:26:55 -0500

    fully functional but not too pretty JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,65 +17,50 @@ $(document).ready(function () {
     $('#crystal-blue').val(blueValue);
     $('#crystal-green').val(greenValue);
 
-    console.log(redValue);
-    console.log(yellowValue);
-    console.log(blueValue);
-    console.log(greenValue);
-
     //add on click events to buttons and add to currentScore
     $("#crystal-red").on("click", function () {
         currentScore = currentScore + parseInt($('#crystal-red').val());
-        console.log(currentScore);
-        console.log(redValue);
-        console.log(yellowValue);
-        console.log(blueValue);
-        console.log(greenValue);
         $('#currentScore').text(currentScore);
     });
 
     $("#crystal-yellow").on("click", function () {
         currentScore = currentScore + parseInt($('#crystal-yellow').val());
-        console.log(currentScore);
-        console.log(redValue);
-        console.log(yellowValue);
-        console.log(blueValue);
-        console.log(greenValue);
         $('#currentScore').text(currentScore);
     });
 
     $("#crystal-blue").on("click", function () {
         currentScore = currentScore + parseInt($('#crystal-blue').val());
-        console.log(currentScore);
-        console.log(redValue);
-        console.log(yellowValue);
-        console.log(blueValue);
-        console.log(greenValue);
         $('#currentScore').text(currentScore);
     });
 
     $("#crystal-green").on("click", function () {
         currentScore = currentScore + parseInt($('#crystal-green').val());
-        console.log(currentScore);
-        console.log(redValue);
-        console.log(yellowValue);
-        console.log(blueValue);
-        console.log(greenValue);
         $('#currentScore').text(currentScore);
     });
 
     //victory and losing conditions/counters
     $("button").on("click", function () {
         if (targetNumber === currentScore) {
-            var ching = new Audio('../sounds/audio_file.mp3');
-            ching.play();
-            winCount = winCount++;
+            document.getElementById('ching').play();
+            winCount = winCount + 1;
             $("#winCount").text(winCount);
-            console.log(winCount)
+            targetNumber = Math.floor((Math.random() * 120) + 19);
+            $('#targetNumber').text(targetNumber);
+            currentScore = 0;
+            $('#currentScore').text(currentScore);
         }
     });
 
-
-    //restart game
-
+    $("button").on("click", function () {
+        if (targetNumber < currentScore) {
+            document.getElementById('aww').play();
+            lossCount = lossCount + 1;
+            $("#lossCount").text(lossCount);
+            targetNumber = Math.floor((Math.random() * 120) + 19);
+            $('#targetNumber').text(targetNumber);
+            currentScore = 0;
+            $('#currentScore').text(currentScore);
+        }
+    });
 
 });
